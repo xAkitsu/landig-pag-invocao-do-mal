4502a6df4625ce3799d225520c295c47fd15214d
Lucas <lucas@gmail.com>
2025-09-10 21:36:01 -0300

    sua mensagem

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,10 @@ const sourcemaps = require("gulp-sourcemaps");
 
 function styles() {
   return gulp.src("src/scss/style.scss")
-    .pipe(sourcemaps.init())
+    //.pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
-    .pipe(cleanCSS())
-    .pipe(sourcemaps.write("."))
+    .pipe(cleanCSS({ level: 2, format: false }))
+    //.pipe(sourcemaps.write("."))
     .pipe(gulp.dest("dist/css"))
 }
 
@@ -18,7 +18,7 @@ function scripts() {
   return gulp.src("src/js/**/*.js")
     .pipe(concat("main.js"))
     .pipe(uglify())
-    .pipe(gulp.dest("dist/js"))
+    .pipe(gulp.dest("dist/js"));
 }
 
 exports.default = gulp.series(gulp.parallel(styles, scripts));
